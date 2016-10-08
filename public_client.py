76419b304289703e95863e5c884b85520c5542e3
Mila432 <Mila432@users.noreply.github.com>
2016-10-08 22:34:54 +0200

    1.0

---
+++ public_client.py
@@ -0,0 +1,67 @@
+import requests
+import json
+import argparse
+
+s = requests.session()
+
+'''
+<-------- short info -------->
+app = the app id: com.google.app
+version = 3.3.5
+map_name = if native code the main lib.so, if unity ..
+pkg_name = same as app
+patch_type = lib native code, unity unity app
+--->patch:
+--->pattern = has to be a list ['0x13 0x37']
+--->patch = the patched bytes 1337
+<---------------------------->
+'''
+
+api_token='1234' #your api token
+s.headers.update({'Content-Type': 'application/json', 'api_token': api_token})
+
+base = 'http://mila432.xyz:5000'
+
+def getCurrentPatches(app):
+    url = base + '/%s/get' % (app)
+    return s.get(url).content
+
+def addNewPatch(app,version):
+    url = base + '/%s/add' % (app)
+    payload = {'app': app,
+               'version': version,
+               'map_name':'lib_app.so',
+               'pkg_name':app,
+               'patch_type':'lib',
+               'patch': [{
+                   'name': 'patch1',
+                   'pattern': ['0x40 0x40 0x40 0x40'],
+                   'patch': ['01234567']
+               }, {
+                   'name': 'patch2',
+                   'pattern': ['0x41 0x41 0x41 0x41'],
+                   'patch': ['76543210']
+               }]}
+    for p in payload['patch']:
+        if '0x40 0x40 0x40 0x40' in p['pattern']:
+            print '[-] default patch... change it'
+            exit(1)
+    return s.post(url, data=json.dumps(payload)).content
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-app', action='store', dest='app', help='App id')
+    parser.add_argument('-version', action='store', dest='version', help='App version')
+    parser.add_argument('-add', dest='add', action='store_true',help='Adds new patch')
+    parser.add_argument('-show', dest='show', action='store_true',help='Show current patches')
+    args= parser.parse_args()
+    if args.app and args.version:
+        if args.add:
+            print addNewPatch(args.app,args.version)
+        if args.show:
+            print getCurrentPatches(args.app)
+    else:
+        print '[-] please provide app version/id'
+
+if __name__ == '__main__':
+    main()
